603da4dca8ba4091f8dd7432bc3332a832bbbf0b
sdin <sdin@LAPTOP-DINESH>
2023-06-01 07:31:04 +0200

    Implement Blob service

--- RedMango_API/Services/IBlobService.cs
+++ RedMango_API/Services/IBlobService.cs
@@ -1,6 +1,10 @@
 ﻿namespace RedMango_API.Services
 {
-    public class IBlobService
+    public interface IBlobService
     {
+        Task<string> GetBlob(string blobName, string containerName);
+        Task<bool> DeleteBlob(string blobName, string containerName);
+        Task<string> UploadBlob(string blobName, string containerName, IFormFile file);
+
     }
 }
