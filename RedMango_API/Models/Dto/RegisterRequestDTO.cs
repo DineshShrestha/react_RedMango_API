aedbd834be5fdebe54f3ea7f7a2adf460e50526d
sdin <sdin@LAPTOP-DINESH>
2023-08-21 09:49:37 +0200

    added login and register controller with jwt token authentication

--- RedMango_API/Models/Dto/RegisterRequestDTO.cs
+++ RedMango_API/Models/Dto/RegisterRequestDTO.cs
@@ -4,7 +4,6 @@ namespace RedMango_API.Models.Dto
 {
     public class RegisterRequestDTO
     {
-        [Required]
         public string UserName { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
--- RedMango_API/Program.cs
+++ RedMango_API/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using RedMango_API.Data;
 using RedMango_API.Models;
 using RedMango_API.Services;
+using System.Runtime;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddSingleton(u => new BlobServiceClient(builder.Configuration.GetConnectionString("StorageAccount")));
 builder.Services.AddSingleton<IBlobService, BlobService>();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 1;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
