395e0ed4f7c7ddc2f80f6343ea797a5e6b0540bf
sdin <sdin@LAPTOP-DINESH>
2023-08-21 10:03:14 +0200

    added authentication and authorization endpoints

---
+++ RedMango_API/Controllers/AuthTestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Utility;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/AuthTest")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetSomething()
+        {
+            return "you are authenticated";
+        }
+
+        [HttpGet("{int:int}")]
+        [Authorize(Roles = SD.Role_Admin)]
+        public async Task<ActionResult<string>> GetSomething(int someIntValue)
+        {
+            //authorization --> Authentication + some access/role
+            return "You are authoriezed with role of admin";
+        }
+    }
+}
