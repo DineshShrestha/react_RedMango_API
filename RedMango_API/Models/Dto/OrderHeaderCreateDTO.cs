4570ab85204e6713c624ab50b60aa0c08ecec19c
sdin <sdin@LAPTOP-DINESH>
2023-08-22 09:31:49 +0200

    Order Header and details model

---
+++ RedMango_API/Models/Dto/OrderHeaderCreateDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RedMango_API.Models.Dto
+{
+    public class OrderHeaderCreateDTO
+    {
+       
+        [Required]
+        public string PickupName { get; set; }
+        [Required]
+        public string PickupPhoneNumber{get; set; }
+        [Required]
+        public int PickupEmail { get; set; }
+        public string ApplicationUserId{get; set;}
+        public double OrderTotal { get; set; }
+
+   
+        public string StripePaymentIntentID{get; set;}
+        public string status{get; set;}
+        public int TotalItems{get; set; }
+
+        public IEnumerable<OrderDetailsCreateDTO> OrderDetailsDTO { get; set; }
+    }
+}
